5f4634e838cafa141ef2ecacd38310e06ddc7ea4
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-10-29 16:59:37 +0100

    Zrobione 3 zadania z laba 4
    updated do poprawek

---
+++ src/main/java/com/java/project/model/kebab/KebabDTO.java
@@ -0,0 +1,38 @@
+package com.java.project.model.kebab;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class KebabDTO {
+    private Integer id;
+    private String name;
+    private String size;
+    private String meat;
+    private String sauce;
+    private double price;
+
+    public KebabDTO(Kebab kebab) {
+        this.id = kebab.getId();
+        this.name = kebab.getName();
+        this.size = kebab.getSize();
+        this.meat = kebab.getMeat();
+        this.sauce = kebab.getSauce();
+        this.price = kebab.getPrice();
+    }
+
+    public Kebab toEntity() {
+        Kebab kebab = new Kebab();
+        kebab.setId(this.id);
+        kebab.setName(this.name);
+        kebab.setSize(this.size);
+        kebab.setMeat(this.meat);
+        kebab.setSauce(this.sauce);
+        kebab.setPrice(this.price);
+        return kebab;
+    }
+}
