23b6012012e328c7369f5809b7241dd882e4389b
Felipe-devv <s97745@pollub.edu.pl>
2024-10-29 16:42:45 +0100

    paragon klasa encyjna dodana z metodą dodającą kebsa do listy

--- src/main/java/com/java/project/model/Paragon.java
+++ src/main/java/com/java/project/model/Paragon.java
@@ -1,4 +1,37 @@
 package com.java.project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Paragon {
+
+    private Integer id;
+    private String miasto;
+    private String kodPocztowy;
+    private List<Kebab> kebaby;
+    private Double suma ;
+
+    public Paragon(String miasto, String kodPocztowy, List<Kebab> kebaby, Double suma) {
+        this.miasto = miasto;
+        this.kodPocztowy = kodPocztowy;
+        this.kebaby = kebaby;
+        this.suma = suma;
+    }
+
+    public void addKebab(Kebab kebab) {
+        if(kebaby.isEmpty())
+        {
+            kebaby = new ArrayList<>();
+        }
+        kebaby.add(kebab);
+    }
 }
