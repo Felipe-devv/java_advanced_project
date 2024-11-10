d5ef0614acaca7835976492ced5415b94ab04f3b
Sprysio <99020634+Sprysio@users.noreply.github.com>
2024-11-10 17:28:09 +0100

    removed unused comments

--- src/main/java/com/java/project/kebab/KebabRepository.java
+++ src/main/java/com/java/project/kebab/KebabRepository.java
@@ -1,10 +1,8 @@
 package com.java.project.kebab;
 
-import java.beans.BeanProperty;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/java/project/paragon/Paragon.java
+++ src/main/java/com/java/project/paragon/Paragon.java
@@ -1,14 +1,10 @@
 package com.java.project.paragon;
 
-import com.java.project.kebab.Kebab;
-import com.java.project.turek.Turek;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
