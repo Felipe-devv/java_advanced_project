08d1eb54c77b614268d3c41e072b3d6e8630aeee
Mac <maciooox3@gmail.com>
2024-11-30 18:47:07 +0100

    fix: service independent

--- src/main/java/com/java/project/turek/TurekController.java
+++ src/main/java/com/java/project/turek/TurekController.java
@@ -3,7 +3,6 @@ package com.java.project.turek;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
